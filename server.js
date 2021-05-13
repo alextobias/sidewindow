10f84cd17d9d6c2c1b1566bd56fdad0f23b6a782
Alex Tobias <alexanderleetobias@gmail.com>
2021-05-13 21:57:43 +0800

    Implemented disconnect events and new event format

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const socketio_server_options = {
     }
 }
 const io_server = new SocketIOServer(http_server, socketio_server_options)
-// now io is the Server instance of socketio
+// now io_server is the Server instance of socketio
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/pub')))
@@ -57,10 +57,23 @@ io_server.on('connection', (socket) => {
         extension_socket.join(room_id)
         console.log(`> This extension socket is in rooms ${String(extension_socket.rooms)}`)
 
+        extension_socket.on("extension:msg", (msg) => {
+            console.log(`> [extension:msg] received message '${msg} from extension ${extension_socket.id}`)
+            console.log(`> [extension:msg] now broadcasting: "${msg}" to room ${room_id}`)
+            extension_socket.to(room_id).emit("extension:msg", msg)
+        })
+
         extension_socket.on("extension-msg", (msg) => {
             console.log(`> [extension-msg] received message: "${msg}" from extension ${extension_socket.id}`)
             console.log(`> [extension-msg] now broadcasting: "${msg}" to room ${room_id}`)
             extension_socket.to(room_id).emit("extension-msg", msg)
+            // io_server.to(room_id).emit("extension-msg", msg)// 
+        })
+
+        extension_socket.on("extension:edits", (content) => {
+            console.log(`> [extension:edits] received new edits from extension ${extension_socket.id}`)
+            console.log(`> [extension:edits] now broadcasting new edits  to room ${room_id}`)
+            extension_socket.to(room_id).emit("extension:edits", content)
             // io_server.to(room_id).emit("extension-msg", msg)// 
         })
 
@@ -71,15 +84,29 @@ io_server.on('connection', (socket) => {
             // io_server.to(room_id).emit("extension-msg", msg)// 
         })
 
+        extension_socket.on("disconnect", (msg) => {
+            // also disconnect all browsers connected to this room
+            console.log(`> [extension:disconnect] now disconnecting sockets in room ${room_id}`)
+            io_server.in(room_id).disconnectSockets(true)
+            // remove the room from the list of rooms
+            console.log(`> [extension:disconnect] now removing room ${room_id}`)
+            rooms = rooms.filter( (r) => r !== room_id)
+            console.log(`> [extension:disconnect] rooms are now: ${rooms}`)
+        })
+
     } else if (socket.handshake.query.type == "browser") {
+
         let browser_socket = socket;
-        console.log("> received connection from browser")
-        console.log("> browser socket id is : " + browser_socket.id)
-        console.log("> checking if any rooms match id")
-        let room_id  = browser_socket.handshake.query.room
+        console.log("> received connection from browser");
+        console.log("> browser socket id is : " + browser_socket.id);
+        console.log("> checking if any rooms match id");
+
+        let room_id  = browser_socket.handshake.query.room;
+
         if (!rooms.includes(room_id)) {
-            console.log(`> No room ${room_id} found.`)
-            browser_socket.emit("server-ack", "No room")
+            console.log(`> No room ${room_id} found.`);
+            browser_socket.emit("server-ack", "No room");
+            console.log(`> Disconnecting browser socket ${browser_socket.id}.`);
             socket.disconnect()
         } else {
             console.log(`> Room ${room_id} match!`)
@@ -88,24 +115,33 @@ io_server.on('connection', (socket) => {
             browser_socket.join(room_id)
             console.log(`> This browser socket is in rooms ${String(browser_socket.rooms)}`)
 
+            // new standard will use "browser:msg" format to refer to a browser client
+            browser_socket.on("browser:msg", (msg) => {
+                console.log(`> [browser:msg] received message: "${msg}" from browser ${browser_socket.id}`)
+                console.log(`> [browser:msg] now broadcasting: "${msg}" to room ${room_id}`)
+                browser_socket.to(room_id).emit("browser:msg", msg)
+            })
+
+            browser_socket.on("browser:edits", (content) => {
+                console.log(`> [browser:edits] received new edits from browser ${browser_socket.id}`);
+                console.log(`> [browser:edits] now broadcasting new edits to room ${room_id}`);
+                browser_socket.to(room_id).emit("browser:edits", msg)
+
+            })
+
             browser_socket.on("client-msg", (msg) => {
-                console.log(`> [client-msg] received message: "${msg}" from browser ${browser_socket.id}`)
-                console.log(`> [client-msg] now broadcasting: "${msg}" to room ${room_id}`)
-                browser_socket.to(room_id).emit("client-msg", msg)
+                console.log(`> [client-msg] received message: "${msg}" from browser ${browser_socket.id}`);
+                console.log(`> [client-msg] now broadcasting: "${msg}" to room ${room_id}`);
+                browser_socket.to(room_id).emit("client-msg", msg);
             })
 
             browser_socket.on("client-edits", (msg) => {
-                console.log(`> [client-edits] received new edits from browser ${browser_socket.id}`)
-                console.log(`> [client-edits] now broadcasting new edits to room ${room_id}`)
-                browser_socket.to(room_id).emit("client-edits", msg)
+                console.log(`> [client-edits] received new edits from browser ${browser_socket.id}`);
+                console.log(`> [client-edits] now broadcasting new edits to room ${room_id}`);
+                browser_socket.to(room_id).emit("client-edits", msg);
             })
         } 
     }
-
-    // socket.on('client-msg', (msg) => {
-    //     console.log("[> io] received client message: " + msg)
-    //     socket.emit("server-msg", "I got your message '" + msg + "'")
-    // })
 })
 
 const port = process.env.PORT || 5000
