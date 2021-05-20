78032a7d6a930aad76a66eeb57c829c8cbee8869
Alex <alexanderleetobias@gmail.com>
2021-05-20 23:09:46 +0800

    Removed theme.js, created MuiStyles.js

---
+++ client/src/MuiStyles.js
@@ -0,0 +1,45 @@
+// MUI-related styling will go in this folder
+// Generally, I'd prefer to do styling using SASS/CSS
+import { createMuiTheme, makeStyles } from  "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+})
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        // backgroundColor: "#263238",
+        backgroundColor: "red",
+        // width: "200px",
+      }
+    },
+    MuiSelect: {
+      root: {
+        color: "red"
+      },
+      select: {
+        color: "blue"
+      },
+      selectMenu: {
+        color: "lightgrey"
+      }
+    }
+  },
+  palette: {
+    primary: {
+      light: '#4f5b62',
+      main: '#263238',
+      dark: '#000a12',
+      contrastText: '#eeeeee',
+    },
+    secondary: {
+      light: '#4f83cc',
+      main: '#01579b',
+      dark: '#002f6c',
+      contrastText: '#eeeeee',
+    },
+  },
+});
+
+export {useStyles as useStyles, theme as theme}
